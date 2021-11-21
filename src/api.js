7def1ec8af473bd3998904357639b3678ca23f73
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-11-21 15:17:56 +0700

    add like and dislike functionality

--- src/api.js
+++ src/api.js
@@ -1,41 +1,37 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const router = express.Router();
 
-const sb = require('@supabase/supabase-js');
-const createClient = sb.createClient
+const sb = require("@supabase/supabase-js");
+const createClient = sb.createClient;
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_API_KEY
-)
+);
 
 /**
  * Get all books
  */
 router.get("/books", async (req, res) => {
-
-  let { data, error } = await supabase
-    .from('books')
-    .select('*')
+  let { data, error } = await supabase.from("books").select("*");
 
   if (error) {
-    res.status(500)
-    res.json(error)
+    res.status(500);
+    res.json(error);
   }
 
   res.json(data);
-
 });
 
 /**
@@ -43,38 +39,65 @@ router.get("/books", async (req, res) => {
  */
 router.post("/books", async (req, res) => {
 
-  console.log(req.body)
-
-  let { data, error } = await supabase
-    .from("books")
-    .insert(req.body)
-    .single()
+  let { data, error } = await supabase.from("books").insert(req.body).single();
 
   if (error) {
-    res.status(500)
-    res.json(error)
-  } 
+    res.status(500);
+    res.json(error);
+  }
 
-  res.json(data)
-})
+  res.json(data);
+});
+
+/**
+ * Increment likeCount for a spesific book by 1 poin
+ */
+ router.patch("/books/like", async (req, res) => {
+
+  const bookId = req.body?.book_id
+
+  let { data, error } = await supabase.rpc('increment', {book_id: bookId})
+
+  if (error) {
+    res.status(500);
+    res.json(error);
+  }
+
+  res.json({status: "success"});
+});
+
+/**
+ * Decrement likeCount for a spesific book by 1 poin
+ */
+ router.patch("/books/dislike", async (req, res) => {
+
+  const bookId = req.body?.book_id
+
+  let { data, error } = await supabase.rpc('decrement', {book_id: bookId})
+
+  if (error) {
+    res.status(500);
+    res.json(error);
+  }
+
+  res.json({status: "success"});
+});
 
 /**
  * Get all countries
  */
- router.get("/countries", async (req, res) => {
-
+router.get("/countries", async (req, res) => {
   let { data, error } = await supabase
-    .from('countries')
-    .select('*')
-    .order('name')
+    .from("countries")
+    .select("*")
+    .order("name");
 
   if (error) {
-    res.status(500)
-    res.json(error)
+    res.status(500);
+    res.json(error);
   }
 
   res.json(data);
-
 });
 
 app.use(`/.netlify/functions/api`, router);
